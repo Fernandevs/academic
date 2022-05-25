3bfaa18c6fce9d49f41af727d1501a41179962f5
Fernandevs <ortizfernando05@gmail.com>
2022-05-24 23:38:44 -0500

    Creación del modelo Student, vistas, forms y urls de éste

--- students/models.py
+++ students/models.py
@@ -1,3 +1,94 @@
+import datetime
+
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
+from academic.options import SEX
+from academic.validators import name_validator
+
+from careers.models import Career
+
+
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(
+        _("first name"),
+        max_length=150,
+        validators=[name_validator]
+    )
+
+    last_name = models.CharField(
+        _("last name"),
+        max_length=150,
+        validators=[name_validator]
+    )
+
+    career = models.ForeignKey(
+        to=Career,
+        on_delete=models.CASCADE
+    )
+
+    plan = models.CharField(
+        _('plan'),
+        max_length=255
+    )
+
+    period = models.CharField(
+        _('period'),
+        max_length=255
+    )
+
+    year = models.DateField(
+        _('year'),
+        default=datetime.datetime.now().year
+    )
+
+    record_number = models.PositiveIntegerField(
+        _('record number'),
+    )
+
+    control_number = models.CharField(
+        _('control_number'),
+        max_length=8
+    )
+
+    school_of_origin = models.CharField(
+        _('school_of_origin'),
+        max_length=255
+    )
+
+    average = models.DecimalField(
+        _('average'),
+        max_digits=4,
+        decimal_places=2
+    )
+
+    place_of_origin = models.CharField(
+        _('place_of_origin'),
+        max_length=255
+    )
+
+    municipality_of_origin = models.CharField(
+        _('municipality_of_origin'),
+        max_length=255
+    )
+
+    sex = models.CharField(
+        _('sex'),
+        max_length=1,
+        choices=SEX
+    )
+
+    age = models.PositiveSmallIntegerField(
+        _('age'),
+    )
+
+    entrance_exam_score = models.DecimalField(
+        _('entrance_exam_score'),
+        max_digits=4,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return self.first_name + self.last_name
--- students/views.py
+++ students/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from students.forms import StudentForm
+from students.models import Student
+
 
 # Create your views here.
+
+class StudentListView(LoginRequiredMixin, ListView):
+    model = Student
+    login_url = reverse_lazy('login')
+    template_name = 'students/read.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de estudiantes'
+
+        return context
+
+
+class StudentCreateView(LoginRequiredMixin, CreateView):
+    model = Student
+    form_class = StudentForm
+    login_url = reverse_lazy('login')
+    template_name = 'students/create.html'
+    success_url = reverse_lazy('list_students')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Crear estudiantes'
+
+        return context
+
+
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Student
+    form_class = StudentForm
+    login_url = reverse_lazy('login')
+    template_name_suffix = '_update_form'
+    template_name = 'students/update.html'
+    success_url = reverse_lazy('list_students')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Actualizar estudiantes'
+
+        return context
+
+
+class StudentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Student
+    login_url = reverse_lazy('login')
+    template_name = 'students/delete.html'
+    success_url = reverse_lazy('list_students')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminar estudiantes'
+
+        return context
