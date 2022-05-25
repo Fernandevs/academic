e71626ea8fcc690f56d0e8d65a3524bafb383f39
Fernandevs <ortizfernando05@gmail.com>
2022-05-24 23:18:17 -0500

    Programación de las vistas del dashboard y los urls de éste

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class DashboardTemplateView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('login')
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Inicio'
+
+        return context
