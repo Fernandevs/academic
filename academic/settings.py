c4133e7ab71a9a46bcf5e1af10b0d3854e5bd6ae
Fernandevs <ortizfernando05@gmail.com>
2022-06-13 23:02:45 -0500

    Refactor Procfile

--- academic/settings.py
+++ academic/settings.py
@@ -151,13 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 django_heroku.settings(locals())
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Media settings
 MEDIA_URL = '/media/'
